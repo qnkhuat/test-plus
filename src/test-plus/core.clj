dba1ab6f18f499720a472def4a9612d8402a2cfa
Ngoc Khuat <qn.khuat@gmail.com>
2022-09-05 11:57:18 +0700

    hitting some rocks but that's okay, let's figure it out later

--- src/test-plus/core.clj
+++ src/test-plus/core.clj
@@ -4,19 +4,84 @@
 
 
 ;; NOTE:
-;; the t/testing will append the test string to t/*testing-contexts*
+;; What we need to do seems to be resolve around the test running functions.
+;; the deftest macro returns a function that we can call
+;; and the `test-var` or run test function just run them as a function
 
 
+;; this is a bit harder than I thought:
+;; - deftest is actually just a macro expands its body as a function
+;; - each `testing` inside is basically just expanding the body, so we have no way to have a post-evaluation to "ignore this body because there is one "only" testing.
+;; - if we want to do this we might have to rewrite how we execute tests, and it's could be not practical
 
-
+;; something to loko into : https://github.com/circleci/circleci.test/blob/master/src/circleci/test.clj
 (t/deftest arithmetic-test
  #p t/*testing-contexts*
- (t/testing "first-level"
-   #p t/*testing-contexts*
-   (t/is (= 1 (inc 0)))
+ (t/underline-testing true "first-level"
+                      #p t/*testing-contexts*
+                      (t/is (= 1 (inc 0)))
 
-   (t/testing "nested"
-     #p t/*testing-contexts*
-     (t/is (= 1 (inc 0))))))
+                      (t/testing "nested"
+                        #p t/*testing-contexts*
+                        (t/is (= 1 (inc 0))))))
+
+(macroexpand-1 '(t/underline-testing true "first-level"
+                                     (println "sup")))
+
+(macroexpand-1 '(t/testing "first-level"
+                  (println "sup")))
+
+(t/deftest sup?
+  (t/underline-testing true "first-level"
+                       (println "sup")))
+
+
+(macroexpand '(t/deftest testing
+                  (macroexpand-1 '(t/testing "no has only"
+                                    (println "SUPPPPPPP")))))
+
+
+
+
+(def ^:dynamic *a* nil)
+
+(def f (fn []
+         (binding [*a* (atom false)]
+           (println *a*))))
+
+(f)
+
+
+
+
+
+(macroexpand '(t/deftest testing2
+                (t/testing.only "has only"
+                                (println t/*has-only*))))
+
+(macroexpand-1 '(t/deftest testing2
+                  (t/testing.only "has only"
+                                  (println t/*has-only*))))
+
+(macroexpand-1 '(t/testing "no has only"
+                       (println t/*has-only*)))
+
+
+(macroexpand-1 '(t/deftest test
+                 (println t/*has-only*)))
+
+(t/deftest test
+  (t/testing "ngoc"
+    (println t/*has-only*))
+
+  (t/testing.only "ngoc"
+    (println t/*has-only*)))
+
+
+
+
+#_(t/run-test-var arithmetic-test)
+
+(t/run-test arithmetic-test)
 
 (vals (ns-interns *ns*))
