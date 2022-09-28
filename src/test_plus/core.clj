f69264859980a203cea7b5d9aa28530089a6853c
Ngoc Khuat <qn.khuat@gmail.com>
2022-09-28 12:27:46 +0700

    tested on metabase but only works at top level testing

--- src/test_plus/core.clj
+++ src/test_plus/core.clj
@@ -20,13 +20,16 @@
   `(binding [t/*testing-contexts* (conj t/*testing-contexts* ~string)]
      (testing* true (fn [] ~@body))))
 
+;; PROBLEMS:
+;; - this does not work if testing are nested,, currently it only walks at the outer most layers
+;; - are we dealing with ns correctly here? the comparision of resolve = ns-resolve *ns* looks weird
 (defmacro deftest+
   [name & body]
   (when t/*load-tests*
     (let [has-only?# (true? (walk/walk
                               first
                               (fn [all]
-                                (some #(= (resolve 't/testing-only) (resolve %)) all))
+                                (some #(= (resolve 'clojure.test/testing-only) (ns-resolve *ns* %)) all))
                               body))]
       `(def ~(vary-meta name assoc :test `(fn []
                                             (binding [*has-only?* ~has-only?#] ~@body)))
@@ -37,3 +40,12 @@
   (intern 'clojure.test (with-meta 'testing-only {:macro true}) @#'testing-only)
   (alter-var-root #'clojure.test/testing (constantly @#'testing+))
   (alter-var-root #'clojure.test/deftest (constantly @#'deftest+)))
+
+(comment
+  (install!)
+
+  (t/deftest what
+    (t/testing-only "yes"
+                    (println "YES"))
+    (t/testing "no"
+      (println "NO"))))
